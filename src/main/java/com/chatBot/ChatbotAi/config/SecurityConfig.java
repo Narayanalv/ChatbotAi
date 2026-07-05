5e0e8fe71cbbb8617d30a82d79c58f06626ee052
Narayana <narayana070203@gmail.com>
2026-07-05 20:34:24 +0530

    feat: add production frontend URL to CORS allowed origins in SecurityConfig

--- src/main/java/com/chatBot/ChatbotAi/config/SecurityConfig.java
+++ src/main/java/com/chatBot/ChatbotAi/config/SecurityConfig.java
@@ -84,6 +84,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("https://chatbotfe-vq8p.onrender.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With")); // ← explicit, not "*"
         configuration.setAllowCredentials(true);
